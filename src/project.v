924a72d570d88c341b226b2683a6f12bdc322920
Angela-jys <a4sha@uwaterloo.ca>
2026-09-13 23:44:56 -0400

    Rename module to tt_um_angelajysdemo

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_angela_jys_demo (
+module tt_um_angelajysdemo (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
